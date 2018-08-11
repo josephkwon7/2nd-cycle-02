a4b378d56e5d74cdb2ec9e436ffbf46f669b2653
josephkwon <devel.kwon@gmail.com>
2018-08-11 15:46:39 +0900

    implement basic mustache example

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,16 +1,18 @@
 package net.slipp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-	private int count = 0;
+//	private int count = 0;
 	
 	@GetMapping("/helloworld")
-	public String welcome() {
-		count++;
-		System.out.println("Welcome Controller !! " + count);
+	public String welcome(String name, int age, Model model) {
+		System.out.println("Name : " + name + " Age : " + age);
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
 		return "welcome";
 	}
 }
